e93a7ec50201b533f06baa93c95561fc14ea58ba
David Roper <ebonhand09@gmail.com>
2011-08-11 03:37:04 +1000

    Lots of instructions

--- source/includes/main-loop.asm
+++ source/includes/main-loop.asm
@@ -9,16 +9,15 @@
 MainLoop		LDD	,X++				; Get instruction into A/B
 			STX	Chip8_PC			; This probably isn't needed
 			STD	Chip8_Instruction		; Keep it.. we need this again
-			LDY	#OpTable
+			LDY	#OpTable-1			; Avoid zero case
 			
 			ANDA	#$F0				; Mask off MMM, leaving just the opcode
 			LSRA
 			LSRA
 			LSRA
-			BNE	@SkipZeroCase
-			JSR	[0,Y]
-			JMP	MainLoop			
-@SkipZeroCase		JSR	[A,Y]			
+			INCA		; Avoid zero case
+
+			JSR	[A,Y]			
 ReturnFromExecute	JMP	MainLoop
 
 			;**** 
@@ -42,7 +41,10 @@ OpTable
 OpTableFor8XYN
 			FDB	Op8XY0_CopyYToX
 			FDB	Op8XY1_XOrYIntoX
-			;FDB	Op8XY2_XAndYIntoX
+			FDB	Op8XY2_XAndYIntoX
+			FDB	Op8XY3_XEorYIntoX
+			FDB	Op8XY4_XPlusYIntoXCarry
+			FDB	Op8XY5_XMinusYIntoXCarry
 			
 
 *************************************************
@@ -103,21 +105,21 @@ Op7XNN_AddToVar			GetOtherHalfOfOpIntoA
 *************************************************
 * 
 *************************************************
-Op8XYN_VariableManipulation	LDY	#OpTableFor8XYN
+Op8XYN_VariableManipulation	LDY	#OpTableFor8XYN-1
 				GetPostByteIntoB
-				BNE	@SkipZeroCase
-				JSR	[0,Y]
-				RTS
-@SkipZeroCase			LSLB			; Since table is a set of double-bytes, offset must be doubled
+				LSLB			; Since table is a set of double-bytes, offset must be doubled
+				INCB
 				JSR	[B,Y]
 				RTS
 				
 				
 ;***
-Op8XY0_CopyYToX			LDY	#Chip8_Vars
+Op8XY0_CopyYToX			LDY	#Chip8_Vars-1
 				GetPreByteIntoB
+				INCB
 				LDB	B,Y
 				GetOtherHalfOfOpIntoA
+				INCA
 				STB	A,Y
 				RTS
 ;***				
@@ -133,9 +135,67 @@ Op8XY1_XOrYIntoX		LDY	#Chip8_Vars-1
 				STA	B,Y
 				RTS
 				
+;***				
+Op8XY2_XAndYIntoX		LDY	#Chip8_Vars-1
+				GetPreByteIntoB
+				INCB		; Avoid zero-case
+				GetOtherHalfOfOpIntoA
+				INCA		; Avoid zero-case
+				PSHS	A
+				LDA	A,Y
+				ANDA	B,Y
+				PULS	B
+				STA	B,Y
+				RTS
 				
+;***				
+Op8XY3_XEorYIntoX		LDY	#Chip8_Vars-1
+				GetPreByteIntoB
+				INCB		; Avoid zero-case
+				GetOtherHalfOfOpIntoA
+				INCA		; Avoid zero-case
+				PSHS	A
+				LDA	A,Y
+				EORA	B,Y
+				PULS	B
+				STA	B,Y
+				RTS				
 				
+;***				
+Op8XY4_XPlusYIntoXCarry		LDY	#Chip8_Vars-1
+				GetOtherHalfOfOpIntoA
+				INCA		; Avoid zero-case
+				PSHS	A
+				CLRB
+				PSHS	B	; Used to clear B without affecting CC
+				GetPreByteIntoB
+				INCB		; Avoid zero-case
+				LDA	A,Y
+				ADDA	B,Y
+				PULS	B	; Clear B
+				ROLB		; Get the carry into B
+				STB	Chip8_VF
+				PULS	B
+				STA	B,Y
+				RTS
 				
+;***				
+Op8XY5_XMinusYIntoXCarry	LDY	#Chip8_Vars-1
+				GetOtherHalfOfOpIntoA
+				INCA		; Avoid zero-case
+				PSHS	A
+				CLRB
+				PSHS	B	; Used to clear B without affecting CC
+				GetPreByteIntoB
+				INCB		; Avoid zero-case
+				LDA	A,Y
+				SUBA	B,Y
+				PULS	B	; Clear B
+				ROLB		; Get the carry into B
+				STB	Chip8_VF
+				PULS	B
+				STA	B,Y
+				RTS			
 				
 				
 *************************************************
