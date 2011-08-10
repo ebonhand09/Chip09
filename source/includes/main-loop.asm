3020ede8a3259c1e041664adf92f0b559fb2a454
David Roper <ebonhand09@gmail.com>
2011-08-11 04:23:54 +1000

    Pre-remove-stupid

--- source/includes/main-loop.asm
+++ source/includes/main-loop.asm
@@ -9,7 +9,7 @@
 MainLoop		LDD	,X++				; Get instruction into A/B
 			STX	Chip8_PC			; This probably isn't needed
 			STD	Chip8_Instruction		; Keep it.. we need this again
-			LDY	#OpTable-1			; Avoid zero case
+			LDY	#OpTable			; Avoid zero case
 			
 			ANDA	#$F0				; Mask off MMM, leaving just the opcode
 			LSRA
@@ -45,6 +45,8 @@ OpTableFor8XYN
 			FDB	Op8XY3_XEorYIntoX
 			FDB	Op8XY4_XPlusYIntoXCarry
 			FDB	Op8XY5_XMinusYIntoXCarry
+			FDB	Op8Xx6_ShiftRightXCarry
+			FDB	Op8XY7_YMinusXIntoXCarry
 			
 
 *************************************************
@@ -83,19 +85,21 @@ Op5XY0_SkipNextIfVarsEqual	JMP	Loop
 *************************************************
 
 *************************************************
-* 
+* Sets VX to NN.
 *************************************************
 Op6XNN_SetVar			GetOtherHalfOfOpIntoA
-				LDY	#Chip8_Vars
+				LDY	#Chip8_Vars-1
+				INCA
 				STB	A,Y
 				RTS		
 *************************************************
 
 *************************************************
-* 
+* Adds NN to VX.
 *************************************************
 Op7XNN_AddToVar			GetOtherHalfOfOpIntoA
-				LDY	#Chip8_Vars
+				LDY	#Chip8_Vars-1
+				INCA
 				LEAY	A,Y
 				ADDB	,Y
 				STB	,Y
@@ -103,17 +107,18 @@ Op7XNN_AddToVar			GetOtherHalfOfOpIntoA
 *************************************************
 
 *************************************************
-* 
+* Various Variable Manipulations - Subtable
 *************************************************
 Op8XYN_VariableManipulation	LDY	#OpTableFor8XYN-1
 				GetPostByteIntoB
 				LSLB			; Since table is a set of double-bytes, offset must be doubled
 				INCB
 				JSR	[B,Y]
-				RTS
+				RTS				
 				
-				
-;***
+*************************************************
+* 0 - Sets VX to the value of VY.
+*************************************************
 Op8XY0_CopyYToX			LDY	#Chip8_Vars-1
 				GetPreByteIntoB
 				INCB
@@ -122,7 +127,10 @@ Op8XY0_CopyYToX			LDY	#Chip8_Vars-1
 				INCA
 				STB	A,Y
 				RTS
-;***				
+				
+*************************************************
+* 1 - Sets VX to VX OR VY.
+*************************************************				
 Op8XY1_XOrYIntoX		LDY	#Chip8_Vars-1
 				GetPreByteIntoB
 				INCB		; Avoid zero-case
@@ -135,7 +143,9 @@ Op8XY1_XOrYIntoX		LDY	#Chip8_Vars-1
 				STA	B,Y
 				RTS
 				
-;***				
+*************************************************
+* 2 - Sets VX to VX AND VY.
+*************************************************			
 Op8XY2_XAndYIntoX		LDY	#Chip8_Vars-1
 				GetPreByteIntoB
 				INCB		; Avoid zero-case
@@ -148,7 +158,9 @@ Op8XY2_XAndYIntoX		LDY	#Chip8_Vars-1
 				STA	B,Y
 				RTS
 				
-;***				
+*************************************************
+* 3 - Sets VX to VX XOR VY.
+*************************************************				
 Op8XY3_XEorYIntoX		LDY	#Chip8_Vars-1
 				GetPreByteIntoB
 				INCB		; Avoid zero-case
@@ -161,7 +173,9 @@ Op8XY3_XEorYIntoX		LDY	#Chip8_Vars-1
 				STA	B,Y
 				RTS				
 				
-;***				
+*************************************************
+* 4 - Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
+*************************************************				
 Op8XY4_XPlusYIntoXCarry		LDY	#Chip8_Vars-1
 				GetOtherHalfOfOpIntoA
 				INCA		; Avoid zero-case
@@ -179,7 +193,9 @@ Op8XY4_XPlusYIntoXCarry		LDY	#Chip8_Vars-1
 				STA	B,Y
 				RTS
 				
-;***				
+*************************************************
+* 5 - VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+*************************************************			
 Op8XY5_XMinusYIntoXCarry	LDY	#Chip8_Vars-1
 				GetOtherHalfOfOpIntoA
 				INCA		; Avoid zero-case
@@ -197,7 +213,58 @@ Op8XY5_XMinusYIntoXCarry	LDY	#Chip8_Vars-1
 				STA	B,Y
 				RTS			
 				
-				
+*************************************************
+* 6 - Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift.
+*************************************************
+Op8Xx6_ShiftRightXCarry		LDY	#Chip8_Vars-1
+				CLRB
+				GetOtherHalfOfOpIntoA
+				INCA		; Avoid zero-case
+				LSR	A,Y
+				ROLB		; Get the carry into B
+				STB	Chip8_VF
+				RTS
+
+*************************************************
+* 7 - Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+*************************************************
+Op8XY7_YMinusXIntoXCarry	LDY	#Chip8_Vars-1
+				GetOtherHalfOfOpIntoA
+				INCA		; Avoid zero-case
+				PSHS	A
+				CLRB
+				PSHS	B	; Used to clear B without affecting CC
+				GetPreByteIntoB
+				INCB		; Avoid zero-case
+				LDB	B,Y
+				SUBB	A,Y
+				PULS	A	; Clear A
+				ROLA		; Get the carry into A
+				STA	Chip8_VF
+				PULS	A
+				STB	A,Y
+				RTS
+
+*************************************************
+* E - Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift.
+*************************************************
+Op8XxE_ShiftLeftXCarry		LDY	#Chip8_Vars-1
+				CLRB
+				GetOtherHalfOfOpIntoA
+				INCA		; Avoid zero-case
+				LSL	A,Y
+				ROLB		; Get the carry into B
+				STB	Chip8_VF
+				RTS
+
+*************************************************
+* 
+*************************************************
+
+
+
+*************************************************
+* 
 *************************************************
 
 
